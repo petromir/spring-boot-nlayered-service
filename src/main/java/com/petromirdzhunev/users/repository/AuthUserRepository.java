feeaa77e186e0dbd7c63eb5722f19538688e0e5e
Petromir Dzhunev <petromir@gmail.com>
2026-06-17 12:02:57 +0300

    feat: Support of multiple user roles

--- src/main/java/com/petromirdzhunev/users/repository/AuthUserRepository.java
+++ src/main/java/com/petromirdzhunev/users/repository/AuthUserRepository.java
@@ -1,21 +1,26 @@
 package com.petromirdzhunev.users.repository;
 
 import static com.petromirdzhunev.users.repository.jooq.Tables.AUTH_USER_DB_TABLE;
+import static com.petromirdzhunev.users.repository.jooq.Tables.AUTH_USER_ROLES_DB_TABLE;
 import static com.petromirdzhunev.users.repository.jooq.Tables.AUTH_USER_ROLE_DB_TABLE;
 import static org.jooq.Records.mapping;
 import static org.jooq.impl.DSL.multiset;
 import static org.jooq.impl.DSL.select;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jooq.Condition;
 import org.jooq.DSLContext;
+import org.jooq.InsertValuesStep2;
+import org.jooq.Record;
 import org.springframework.stereotype.Component;
 
-import com.petromirdzhunev.users.entity.AuthUser;
-import com.petromirdzhunev.users.entity.AuthUserRole;
 import com.petromirdzhunev.exception.EntityAlreadyExistsException;
 import com.petromirdzhunev.exception.EntityNotFoundException;
+import com.petromirdzhunev.users.entity.AuthUser;
+import com.petromirdzhunev.users.entity.AuthUserRole;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,26 +31,39 @@ public class AuthUserRepository {
 	private final DSLContext db;
 
 	public AuthUser insertAuthUser(final AuthUser authUser) {
-		// NOTE: Support multiple roles
-		final String authUserRoleCode = authUser.getRoles().getFirst().code();
-		final long authUserRoleId = db.select(AUTH_USER_ROLE_DB_TABLE.ID)
-		                              .from(AUTH_USER_ROLE_DB_TABLE)
-		                              .where(AUTH_USER_ROLE_DB_TABLE.CODE.eq(authUserRoleCode))
-		                              .fetchOptional(AUTH_USER_ROLE_DB_TABLE.ID)
-		                              .orElseThrow(() -> new EntityNotFoundException(
-				                                    "No existing auth user role found for [code=%s]".formatted(
-						                                    authUserRoleCode)));
+		final List<String> roleCodes = authUser.getRoles()
+		                                       .stream()
+		                                       .map(AuthUserRole::code)
+		                                       .toList();
 
-		return db.insertInto(AUTH_USER_DB_TABLE)
-		         .set(AUTH_USER_DB_TABLE.EMAIL, authUser.getEmail())
-		         .set(AUTH_USER_DB_TABLE.PASSWORD, authUser.getPassword())
-		         .set(AUTH_USER_DB_TABLE.FIRST_NAME, authUser.getFirstName())
-		         .set(AUTH_USER_DB_TABLE.LAST_NAME, authUser.getLastName())
-		         .set(AUTH_USER_DB_TABLE.ROLE_ID, authUserRoleId)
-		         .returning(AUTH_USER_DB_TABLE.ID)
-		         .fetchOne(record -> AuthUser.builder()
-		                                     .id(record.getValue(AUTH_USER_DB_TABLE.ID))
-		                                     .build());
+		final List<Long> roleIds = db.select(AUTH_USER_ROLE_DB_TABLE.ID)
+		                             .from(AUTH_USER_ROLE_DB_TABLE)
+		                             .where(AUTH_USER_ROLE_DB_TABLE.CODE.in(roleCodes))
+		                             .fetch(AUTH_USER_ROLE_DB_TABLE.ID);
+
+		if (roleIds.isEmpty()) {
+			throw new EntityNotFoundException(
+					"No auth user role found [codes=%s]".formatted(StringUtils.join(roleCodes, ",")));
+		}
+
+		final Long authUserId = db.insertInto(AUTH_USER_DB_TABLE)
+		                          .set(AUTH_USER_DB_TABLE.EMAIL, authUser.getEmail())
+		                          .set(AUTH_USER_DB_TABLE.PASSWORD, authUser.getPassword())
+		                          .set(AUTH_USER_DB_TABLE.FIRST_NAME, authUser.getFirstName())
+		                          .set(AUTH_USER_DB_TABLE.LAST_NAME, authUser.getLastName())
+		                          .returning(AUTH_USER_DB_TABLE.ID)
+		                          .fetchOptional(AUTH_USER_DB_TABLE.ID)
+		                          .orElseThrow(() -> new EntityNotFoundException(
+				                          "Auth user not inserted [email=%s]".formatted(authUser.getEmail())));
+
+		InsertValuesStep2<Record, Long, Long> insertStep = db.insertInto(
+				AUTH_USER_ROLES_DB_TABLE, AUTH_USER_ROLES_DB_TABLE.USER_ID, AUTH_USER_ROLES_DB_TABLE.ROLE_ID);
+		for (final Long roleId : roleIds) {
+			insertStep = insertStep.values(authUserId, roleId);
+		}
+		insertStep.execute();
+
+		return authUser.withoutPassword();
 	}
 
 	public AuthUser authUser(final String authUserEmail) {
@@ -54,9 +72,9 @@ public class AuthUserRepository {
 						.formatted(authUserEmail)));
 	}
 
-	public AuthUser authUser(final Long userId) {
-		return this.authUser(AUTH_USER_DB_TABLE.ID.eq(userId),
-				() -> new EntityNotFoundException("No existing auth user found for [id=%d]".formatted(userId)));
+	public AuthUser authUser(final Long authUserId) {
+		return this.authUser(AUTH_USER_DB_TABLE.ID.eq(authUserId),
+				() -> new EntityNotFoundException("No existing auth user found for [id=%d]".formatted(authUserId)));
 	}
 
 	public void deleteAuthUser(final Long authUserId) {
@@ -65,12 +83,12 @@ public class AuthUserRepository {
 		  .execute();
 	}
 
-	public void assertAuthUserNotExists(final String email) {
+	public void assertAuthUserNotExists(final String authUserEmail) {
 		boolean flowExists =
 				db.fetchExists(
 						db.selectOne()
 						  .from(AUTH_USER_DB_TABLE)
-						  .where(AUTH_USER_DB_TABLE.EMAIL.eq(email)));
+						  .where(AUTH_USER_DB_TABLE.EMAIL.eq(authUserEmail)));
 
 		if (flowExists) {
 			throw new EntityAlreadyExistsException("Username already exists for [name=%s]");
@@ -79,23 +97,24 @@ public class AuthUserRepository {
 
 	private AuthUser authUser(final Condition condition, final Supplier<EntityNotFoundException> exceptionSupplier) {
 		return db.select(AUTH_USER_DB_TABLE.ID,
-				         AUTH_USER_DB_TABLE.EMAIL,
-				         AUTH_USER_DB_TABLE.PASSWORD,
-				         AUTH_USER_DB_TABLE.FIRST_NAME,
-				         AUTH_USER_DB_TABLE.LAST_NAME,
-				         multiset(
-						         select(
-								         AUTH_USER_ROLE_DB_TABLE.ID, AUTH_USER_ROLE_DB_TABLE.CODE, AUTH_USER_ROLE_DB_TABLE.NAME
-						         ).from(AUTH_USER_ROLE_DB_TABLE)
-						          .where(AUTH_USER_ROLE_DB_TABLE.ID.eq(AUTH_USER_DB_TABLE.ROLE_ID))
-				         ).as("stepGroups")
-				          .convertFrom(r -> r.map(record ->
-								          new AuthUserRole(
-										          record.getValue(AUTH_USER_ROLE_DB_TABLE.ID),
-										          record.getValue(AUTH_USER_ROLE_DB_TABLE.CODE),
-										          record.getValue(AUTH_USER_ROLE_DB_TABLE.NAME))
-						          )
-				          ))
+			         AUTH_USER_DB_TABLE.EMAIL,
+			         AUTH_USER_DB_TABLE.PASSWORD,
+			         AUTH_USER_DB_TABLE.FIRST_NAME,
+			         AUTH_USER_DB_TABLE.LAST_NAME,
+			         multiset(
+				         select(
+					         AUTH_USER_ROLE_DB_TABLE.ID, AUTH_USER_ROLE_DB_TABLE.CODE, AUTH_USER_ROLE_DB_TABLE.NAME
+				         ).from(AUTH_USER_ROLES_DB_TABLE)
+				          .join(AUTH_USER_ROLE_DB_TABLE).on(AUTH_USER_ROLE_DB_TABLE.ID.eq(AUTH_USER_ROLES_DB_TABLE.ROLE_ID))
+						         .and(AUTH_USER_ROLES_DB_TABLE.USER_ID.eq(AUTH_USER_DB_TABLE.ID))
+			         ).as("roles")
+			          .convertFrom(r -> r.map(record ->
+					          new AuthUserRole(
+						          record.getValue(AUTH_USER_ROLE_DB_TABLE.ID),
+						          record.getValue(AUTH_USER_ROLE_DB_TABLE.CODE),
+						          record.getValue(AUTH_USER_ROLE_DB_TABLE.NAME))
+				          )
+			          ))
 		         .from(AUTH_USER_DB_TABLE)
 		         .where(condition)
 		         .fetchOptional()
